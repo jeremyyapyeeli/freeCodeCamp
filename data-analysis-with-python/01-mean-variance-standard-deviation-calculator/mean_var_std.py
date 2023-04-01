0f1cb38c0a67764970c445344fdcfc1605897310
Jeremy Yap Yee Li <j.yapyeeli@gmail.com>
2023-04-01 17:39:01 +0800

    removed unnecessary .tolist() calls

--- data-analysis-with-python/01-mean-variance-standard-deviation-calculator/mean_var_std.py
+++ data-analysis-with-python/01-mean-variance-standard-deviation-calculator/mean_var_std.py
@@ -11,32 +11,32 @@ def calculate(list):
   calculations['mean'] = [
     df_list.mean(axis=0).tolist(),
     df_list.mean(axis=1).tolist(),
-    df_list.mean().tolist()
+    df_list.mean()
   ]
   calculations['variance'] = [
     df_list.var(axis=0).tolist(),
     df_list.var(axis=1).tolist(),
-    df_list.var().tolist()
+    df_list.var()
   ]
   calculations['standard deviation'] = [
     df_list.std(axis=0).tolist(),
     df_list.std(axis=1).tolist(),
-    df_list.std().tolist()
+    df_list.std()
   ]
   calculations['max'] = [
     df_list.max(axis=0).tolist(),
     df_list.max(axis=1).tolist(),
-    df_list.max().tolist()
+    df_list.max()
   ]
   calculations['min'] = [
     df_list.min(axis=0).tolist(),
     df_list.min(axis=1).tolist(),
-    df_list.min().tolist()
+    df_list.min()
   ]
   calculations['sum'] = [
     df_list.sum(axis=0).tolist(),
     df_list.sum(axis=1).tolist(),
-    df_list.sum().tolist()
+    df_list.sum()
   ]
 
   return calculations
